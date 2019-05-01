0dc7f2274bf81cc0c452acf5dc2eb8cac5301bbc
Kola <Nik@Apples-iMac-179.local>
2019-05-01 02:23:36 -0700

    strcmp

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -15,13 +15,11 @@ int		ft_strcmp(char *s1, char *s2)
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] || s2[i])
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
-	if ((s1[i] == '\0' && s2[i] != '\0') || (s1[i] != '\0' && s2[i] == '\0'))
-		return (s1[i] - s2[i]);
 	return (0);
 }
--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -15,7 +15,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
