d0b389f96674119a40c653c599b7d4c1975c2f50
Nikolai Vinogradov <vinograd@e1z2r7p24.42.us.org>
2019-05-03 18:23:09 -0700

    libft

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: vinograd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 17:11:09 by vinograd          #+#    #+#             */
-/*   Updated: 2019/05/03 17:52:16 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/05/03 18:14:44 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-		unsigned char *p_dst;
-		unsigned char *p_src;
+	unsigned char *p_dst;
+	unsigned char *p_src;
 
-		p_dst = (unsigned char*)dst;
-		p_src = (unsigned char*)src;
-		if (dst >= src)
-			while (len--)
-				p_dst[len] = p_src[len];
-		else
-			while (len--)
-			{
-				*p_dst = *p_src;
-				p_src++;
-				p_dst++;
-			}
-		return (dst);
+	p_dst = (unsigned char*)dst;
+	p_src = (unsigned char*)src;
+	if (dst >= src)
+		while (len--)
+			p_dst[len] = p_src[len];
+	else
+		while (len--)
+		{
+			*p_dst = *p_src;
+			p_src++;
+			p_dst++;
+		}
+	return (dst);
 }
