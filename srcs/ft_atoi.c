287232173d742b19ed351106dcf76e1e3998aa1b
Nikolai Vinogradov <vinograd@e1z2r7p24.42.us.org>
2019-05-03 19:02:06 -0700

    .

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,27 +6,20 @@
 /*   By: vinograd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 17:23:37 by vinograd          #+#    #+#             */
-/*   Updated: 2019/05/03 14:08:46 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/05/03 18:58:30 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include <limits.h>
-
-long long		ft_atoi(const char *s)
+int		ft_atoi(const char *s)
 {
 	int sign;
-	long long res;
+	int res;
 	int i;
-//	long long min = -9223372036854775808;
-//	long long max = 9223372036854775807;
-	long long check;
 
-	check = 0;
 	res = 0;
 	sign = 1;
 	i = 0;
-	while (s[i] >= 0 && s[i] <= 32 && s[i] != '\0')
+	while ((s[i] >= 9 && s[i] <= 19) || s[i] == ' ')
 		i++;
 	if (s[i] == '-')
 	{
@@ -36,35 +29,6 @@ long long		ft_atoi(const char *s)
 	else if (s[i] == '+')
 		i++;
 	while ((s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
-	{
-		if (check > LONG_MAX)
-			return (-1);
-		if (check < LONG_MIN)
-			return (0);
-		res = res * 10 + (s[i] - '0');
-		check = check * 10 + (s[i] - '0');
-		i++;
-	}
-
+		res = res * 10 + (s[i++] - '0');
 	return (res * sign);
 }
-
-#include <stdlib.h>
-
-int ft_atoi2(const char *s)
-{
-	return (atoi(s));
-}
-
-#include <stdio.h>
-int main()
-{
-	printf("%ld\n", LONG_MAX);
-	printf("%ld\n", LONG_MIN);
-	char str[] = "-9223372036854775810";
-	printf("arigin\t%d\n", ft_atoi2(str));
-	printf("mine\t%lld\n", ft_atoi(str));
-
-}
-
-
--- srcs/libft.h
+++ srcs/libft.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   libft2.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vinograd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/30 15:19:48 by vinograd          #+#    #+#             */
-/*   Updated: 2019/05/03 18:04:12 by vinograd         ###   ########.fr       */
+/*   Created: 2019/05/03 18:26:04 by vinograd          #+#    #+#             */
+/*   Updated: 2019/05/03 18:37:07 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,41 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <stddef.h>
 
-void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memcpy(void *restrict dst, void *restrict src, size_t n);
+void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(int i);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+char	*ft_itoa(int nbr);
 void	*ft_memccpy(void *restrict dst, const void *restrict src, \
 		int c, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-char	**ft_strsplit(char const *str, char c);
-int		ft_atoi(const char *s);
-void	ft_bzero(void *s, size_t n);
-char	*ft_itoa(int nbr);
+void	*ft_memcpy(void *restrict dst, void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
 void	ft_putchar(char c);
 void	ft_putnbr(int n);
 void	ft_putstr(char const *s);
-void	ft_strcat(char *str1, char *str2);
-void	ft_strncat(char *str1, char*str2, int n);
+char	*ft_strcat(char *restrict str1, const char *restrict str2);
+char	*ft_strchr(const char *s, int c);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dst, const char *src);
-char	*ft_strncpy(char *dest, char *src, unsigned int n);
 char	*ft_strdup(char *s);
+size_t	ft_strlcat(char *restrict dst, const char \
+		*restrict src, size_t dstsize);
 size_t	ft_strlen(char *s);
-char	*ft_strstr(char *haystach, char *needle);
+char	*ft_strncat(char *str1, const char *str2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	**ft_strsplit(char const *str, char c);
+char	*ft_strstr(const char *haystach, const char *needle);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_isalnum(int c);
-int		ft_isdigit(int c);
-int		ft_isalpha(int c);
-int		ft_strcmp(char *s1, char *s2);
-int		ft_strncmp(char *s1, char *s2, unsigned int n);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
 
 #endif
